a4f27ef4af694885f140c48fc68f7d98a60fe554
Namr <amrmoussa100@gmail.com>
2018-02-27 23:48:04 -0500

    Updated Robotmap

--- src/org/usfirst/frc/team4795/robot/RobotMap.java
+++ src/org/usfirst/frc/team4795/robot/RobotMap.java
@@ -2,7 +2,7 @@ package org.usfirst.frc.team4795.robot;
 
 public enum RobotMap {
 	// CAN motor controllers
-	LEFT_MOTOR_1(5), LEFT_MOTOR_2(3), RIGHT_MOTOR_1(6), RIGHT_MOTOR_2(2), ARM_MOTOR(1), PCM(20),
+	LEFT_MOTOR_1(5), LEFT_MOTOR_2(3), RIGHT_MOTOR_1(6), RIGHT_MOTOR_2(2), ARM_MOTOR(1), PCM(0),
 	//PCM Mappings
 	PCM_SHOOTER_FORWARD(6), PCM_SHOOTER_REVERSE(4),
 	// PWM Mappings
